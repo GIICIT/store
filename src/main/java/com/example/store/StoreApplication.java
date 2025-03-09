4988cd737e4dd94ec7e9a60f148d071fd46b665e
vanzyl-wallace <apeiron.technologies@outlook.com>
2025-03-09 20:50:17 +0200

    open-api-update

--- src/main/java/com/example/store/StoreApplication.java
+++ src/main/java/com/example/store/StoreApplication.java
@@ -1,7 +1,5 @@
 package com.example.store;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
